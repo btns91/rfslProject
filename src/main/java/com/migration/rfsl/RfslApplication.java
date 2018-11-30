081231bdf0dca15f0970bfb9a3e0429d1eaec5ac
btns91 <44157226+btns91@users.noreply.github.com>
2018-11-30 11:41:52 +0200

    basicAuth

--- src/main/java/com/migration/rfsl/RfslApplication.java
+++ src/main/java/com/migration/rfsl/RfslApplication.java
@@ -1,39 +1,64 @@
 package com.migration.rfsl;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.migration.rfsl.model.Planets;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+//import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
-public class RfslApplication {
+import com.migration.rfsl.model.Planets;
+import org.apache.commons.codec.binary.Base64;
 
-    ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) {
-        RfslApplication app = new RfslApplication();
+ import com.fasterxml.jackson.databind.ObjectMapper;
 
-        try {
-            Planets planets = app.getPlanets();
 
-            planets.getResults().forEach(p -> System.out.println(p.getName()));
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+ public class  RfslApplication {
+     ObjectMapper mapper = new ObjectMapper();
 
-    private Planets getPlanets() throws IOException {
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet("https://swapi.co/api/planets");
-        request.setHeader(HttpHeaders.ACCEPT, "application/json");
-        HttpResponse response = client.execute(request);
+     public static void main(String[] args) {
+         RfslApplication app = new RfslApplication();
+         try {
+             Planets planets = app.getPlanets();
+             planets.getResults().forEach(p -> System.out.println(p.getName()));
 
-        return mapper.readValue(response.getEntity().getContent(), Planets.class);
-    }
-}
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+
+
+         private Planets getPlanets() throws IOException {
+             String webPage = "http://localhost:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/-root-/children ";
+             String name = "admin";
+             String password = "hiziggyc";
+
+             String authString = name + ":" + password;
+             System.out.println("auth string: " + authString);
+             byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+             String authStringEnc = new String(authEncBytes);
+             System.out.println("Base64 encoded auth string: " + authStringEnc);
+
+             URL url = new URL(webPage);
+             URLConnection urlConnection = url.openConnection();
+             urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+             InputStream is = urlConnection.getInputStream();
+             InputStreamReader isr = new InputStreamReader(is);
+
+             int numCharsRead;
+             char[] charArray = new char[1024];
+             StringBuffer sb = new StringBuffer();
+             while ((numCharsRead = isr.read(charArray)) > 0) {
+                 sb.append(charArray, 0, numCharsRead);
+             }
+             String result = sb.toString();
+
+             //System.out.println("*** BEGIN ***");
+             //System.out.println(result);
+             //System.out.println("*** END ***");
+             return mapper.readValue(result.getEntity().getContent(), Planets.class);
+         }
+
+     }
